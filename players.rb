f53e5612919f27d3c5de4c317bf8b93133fbbdf0
KAKZBI <enelibrice@yahoo.fr>
2026-02-01 07:11:35 +0100

    created a guess function in class HumanPlayer

--- players.rb
+++ players.rb
@@ -3,11 +3,12 @@ class Player
 end
 
 class HumanPlayer < Player
-
+  def guess
+    gets.chomp.to_i
+  end
 end
 
 class ComputerPlayer < Player
-
   def make_code
     @code = (1..4).reduce(''){|str, i| str + rand(1..6).to_s}
   end 
